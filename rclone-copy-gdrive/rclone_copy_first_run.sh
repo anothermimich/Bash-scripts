7d2e192d1c6f9648221d587dbddcc9c94daec72e
anothermimich <lucas.immich@outlook.com>
2024-12-11 20:05:24 -0300

    what

--- rclone-copy-gdrive/rclone_copy_first_run.sh
+++ rclone-copy-gdrive/rclone_copy_first_run.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")/rclone_variables.sh"
 #
 # Based on markuscraig/sync_gdrive.py and on Faris Khasawneh scripts
 
-echo "$(date +'%Y/%m/%d %H:%M:%S') Sync started" >> "$HOME/.config/rclone/rclone.log"
+echo "$(date +'%Y/%m/%d %H:%M:%S') Sync started"
 
 if  [[  "${#LOCAL_DIR[@]}" == "${#REMOTE_DIR[@]}" ]]; then
   for ((i = 0 ; i < "${#REMOTE_DIR[@]}"; i++)); do
@@ -20,7 +20,6 @@ if  [[  "${#LOCAL_DIR[@]}" == "${#REMOTE_DIR[@]}" ]]; then
       --drive-skip-shortcuts \
       --drive-skip-dangling-shortcuts \
       --metadata \
-      --log-file "$HOME/.config/rclone/rclone.log" \
       --retries $RCLONE_RETRIES \
       --checkers $RCLONE_CHECKERS \
       --transfers $RCLONE_TRANFERS \
@@ -28,23 +27,17 @@ if  [[  "${#LOCAL_DIR[@]}" == "${#REMOTE_DIR[@]}" ]]; then
       --drive-chunk-size $RCLONE_CHUNK_SIZE \
       --drive-upload-cutoff $RCLONE_UPLOAD_CUTOFF \
       --check-access \
-      --update 
-    #  Don't work
-    #  --recover \
-    #  --resilient \
-    #  --track-renames \
-    #  --create-empty-src-dirs \
-    #  --fix-case \
-    #  --max-lock 2m \
+      --resync \
+      --verbose
 
     sleep 1
 
     if [ $? -eq 0 ]; then
-      echo "$(date +'%Y/%m/%d %H:%M:%S') Sync done ${i}" >> "$HOME/.config/rclone/rclone.log"
+      echo "$(date +'%Y/%m/%d %H:%M:%S') Sync done ${i}"
     else
-      echo "$(date +'%Y/%m/%d %H:%M:%S') Sync failed  ${i}" >> "$HOME/.config/rclone/rclone.log"
+      echo "$(date +'%Y/%m/%d %H:%M:%S') Sync failed  ${i}"
     fi
   done
 else
-  echo "$(date +'%Y/%m/%d %H:%M:%S') Sintax dir error" >> "$HOME/.config/rclone/rclone.log"
+  echo "$(date +'%Y/%m/%d %H:%M:%S') Sintax dir error"
 fi
