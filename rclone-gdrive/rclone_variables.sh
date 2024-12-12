35f6abf53dc773ee822157e72013ce6db8764b60
anothermimich <lucas.immich@outlook.com>
2024-12-12 09:54:17 -0300

    new entry

--- rclone-gdrive/rclone_variables.sh
+++ rclone-gdrive/rclone_variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # rclone_variables.sh
 
-REMOTE_DIR=('g_drive:/Codes'  'g_drive:/Design')
-LOCAL_DIR=('/home/lu/Codes' '/home/lu/Design') 
+REMOTE_DIR=('g_drive:/Codes'  'g_drive:/Design' 'g_drive:/Zotero')
+LOCAL_DIR=('/home/lu/Codes' '/home/lu/Design' '/home/lu/Zotero') 
 RCLONE_RETRIES=3
 RCLONE_CHECKERS=4
 RCLONE_TRANFERS=8
