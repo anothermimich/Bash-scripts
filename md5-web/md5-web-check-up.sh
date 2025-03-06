cecefa26828eefbbe49edc6d159e52441beac55d
anothermimich <lucas.immich@outlook.com>
2025-03-06 16:04:52 -0300

    ops

--- md5-web/md5-web-check-up.sh
+++ md5-web/md5-web-check-up.sh
@@ -10,8 +10,8 @@ for ((i = 0 ; i < "${#URL[@]}"; i++)); do
 	curl --silent ${URL[i]} | md5sum > check${i}.md5new
 
 	if ! cmp check${i}.md5 check${i}.md5new > /dev/null; then
-		echo "Go check ${URL[i]}" | mail -s "Change detected - MD5 Check " lu.immich@gmail.com
-		echo "Go check ${URL[i]}" | mail -s "Change detected - MD5 Check " alinedalpont68@gmail.com
+		echo "Go check ${URL[i]}" | mail -s "Change detected - MD5 Check " user@mail.com
+		echo "Go check ${URL[i]}" | mail -s "Change detected - MD5 Check " user@mail.com
 		echo -e "Go check ${URL[i]}" 
 		mv check${i}.md5new check${i}.md5
 	else
