18184022b7d128b633151812faebd06da5647789
anothermimich <lucas.immich@outlook.com>
2025-03-07 14:49:27 -0300

    cache and no cache variables

--- md5-web/md5-web-check-up.sh
+++ md5-web/md5-web-check-up.sh
@@ -7,6 +7,8 @@ echo
 echo "--- MD5 fingerprint ---"
 echo 
 
+# No cache
+
 for ((i = 0 ; i < "${#URL[@]}"; i++)); do
 	if [ ! -f check${i}.md5 ]; then
 		curl --silent ${URL[i]} | md5sum > check${i}.md5
@@ -16,9 +18,22 @@ for ((i = 0 ; i < "${#URL[@]}"; i++)); do
 	fi
 done
 
+# With cache
+
+for ((i = 0 ; i < "${#URLC[@]}"; i++)); do
+	if [ ! -f checkcache${i}.md5 ]; then
+		curl --silent ${URLC[i]} | md5sum > checkcache${i}.md5
+		echo -e "Creating a new fingerprint for ${URLC[i]}"
+	else
+		echo -e "Nothing to do"
+	fi
+done
+
 echo 
 echo "--- MD5 Check ---"
 echo 
+
+# No cache
 
 for ((i = 0 ; i < "${#URL[@]}"; i++)); do
 	curl --silent "${URL[i]}?$(date +%s)" | md5sum > check${i}.md5new
@@ -31,5 +46,21 @@ for ((i = 0 ; i < "${#URL[@]}"; i++)); do
 	else
 		echo -e "No changes detected in ${URL[i]}"
 		rm check${i}.md5new
+	fi
+done
+
+# With cache
+
+for ((i = 0 ; i < "${#URLC[@]}"; i++)); do
+	curl --silent "${URLC[i]}" | md5sum > checkcache${i}.md5new
+	if ! cmp checkcache${i}.md5 checkcache${i}.md5new > /dev/null; then
+		for ((c = 0 ; c < "${#MAIL[@]}"; c++)); do
+			echo "Go check ${URLC[i]}" | mail -s "Change detected - MD5 Check " ${MAIL[c]}
+		done
+		echo -e "Go check ${URLC[i]}" 
+		mv checkcache${i}.md5new checkcache${i}.md5
+	else
+		echo -e "No changes detected in ${URLC[i]}"
+		rm checkcache${i}.md5new
 	fi
 done
--- md5-web/md5-web-variables.sh
+++ md5-web/md5-web-variables.sh
@@ -2,4 +2,11 @@
 
 # Don't change the order of the list, if you do it you may receive some faulty output.
 
+
+# No cache
+
 URL=('www.ufsm.br/pro-reitorias/prpgp/editais/065-2024') 
+
+# With cache
+
+URLC=('https://www.ufsm.br/pro-reitorias/prpgp/teslle/busca?q=&area=editais&orderby=date&sites%5B%5D=368&sites%5B%5D=342&sites%5B%5D=818&tags=teslle,%20sufici%C3%AAncia,%20teste%20de%20sufici%C3%AAncia%20em%20l%C3%ADngua%20estrangeira') 
