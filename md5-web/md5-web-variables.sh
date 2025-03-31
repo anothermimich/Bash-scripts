5aac7ecf8f7bd67f5e3b3b1af7a7d70a669aa50d
anothermimich <lucas.immich@outlook.com>
2025-03-31 14:33:44 -0300

    zotero

--- md5-web/md5-web-variables.sh
+++ md5-web/md5-web-variables.sh
@@ -9,4 +9,4 @@ URL=('www.ufsm.br/pro-reitorias/prpgp/editais/065-2024')
 
 # With cache
 
-URLC=('https://www.ufsm.br/pro-reitorias/prpgp/teslle/busca?q=&area=editais&orderby=date&sites%5B%5D=368&sites%5B%5D=342&sites%5B%5D=818&tags=teslle,%20sufici%C3%AAncia,%20teste%20de%20sufici%C3%AAncia%20em%20l%C3%ADngua%20estrangeira' 'https://www.ufsm.br/pro-reitorias/prpgp/teslle') 
+URLC=('') 
